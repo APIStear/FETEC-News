f90a7d44ae440d4ef790e37104f301a223307445
Adrian-Garcia <adriangarlop@hotmail.com>
2021-05-17 19:00:47 -0500

    Added basic model and some functions

--- server/models/user.js
+++ server/models/user.js
@@ -6,11 +6,37 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: [true, "Nombre es requerido"],
   },
-  // matricula
-  // TODO: validation A00000000
   studentId:{
     type: String,
     required: [true, "Matrícula es requerida"],
+  },
+  mail: {
+    type: String,
+    required: [true, "Correo es requerido"],
+  },
+  gender: {
+    type: String
+  },
+  careerProgram: {
+    type: String,
+    required: [true, "Carrera es requerida"],
+  },
+  semester: {
+    type: Number,
+    requred: [true, "Semestre es requerido"],
+  },
+  isTec21: {
+    type: Boolean,
+  },
+  schoolProgram: {
+    type: String
+  },
+  // used to logically delete data, DO NOT EXPOSE OUTSIDE OF BACKEND
+  bActive: {
+    type: Boolean,
+    default: true,
+    // by default, it will not be shown in document, unless selected
+    select: false,
   }
 });
 
@@ -21,5 +47,45 @@ userSchema.path('studentID').validate(function (value) {
 }, 
 "La matrícula debe cumplir con el formato completo. [A0.......]."
 );
+
+userSchema.statics.updateUser = async function(studentId, name, mail, gender, careerProgram, semester, isTec21, schoolProgram) {
+  const user = await this.findOneAndUpdate(
+    {_id: studentId, bActive: true},
+    {
+      name,
+      mail,
+      gender,
+      careerProgram,
+      semester,
+      isTec21,
+      schoolProgram,
+    },
+    {new: true}
+  ).exec();
+
+  if(!user) {
+    return Promise.reject(new MyError(404, "No se encontró el usuario."));
+  }
+
+  return user;
+}
+
+userSchema.statics.deleteUser = async function(studentId) {
+  const user = await this.findOneAndUpdate(
+    {_id: studentId, bActive: true},
+    {bActive: false},
+    {new: true}
+  ).exec()
+
+  if(!user) {
+    return Promise.reject(new MyError(404, "No se encontró el usuario."));
+  }
+
+  return user;
+}
+
+userSchema.statics.getAll = async function(page, pageSize) {
+  
+}
 
 module.exports = mongoose.Model('User', userSchema);
