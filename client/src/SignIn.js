49547559b723186d49f183c71a6e0e35f31a6eae
Memo Saldana <memo.sal98@gmail.com>
2021-05-21 04:37:56 -0500

    Adds navbar and footer, and finishes login design

--- client/src/SignIn.js
+++ client/src/SignIn.js
@@ -1,14 +1,7 @@
 import React from 'react';
-import Avatar from '@material-ui/core/Avatar';
-import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
-import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
-import Link from '@material-ui/core/Link';
 import Grid from '@material-ui/core/Grid';
 import Box from '@material-ui/core/Box';
-import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
@@ -16,26 +9,22 @@ import GoogleLogin from 'react-google-login';
 import axios from "axios";
 import { Paper } from '@material-ui/core';
 
-function Copyright() {
-  return (
-    <Typography variant="body2" color="textSecondary" align="center">
-      {'Copyright © '}
-      <Link color="inherit" href="https://material-ui.com/">
-        Comite Electoral
-      </Link>{' '}
-      {new Date().getFullYear()}
-      {'.'}
-    </Typography>
-  );
-}
-
-
 const useStyles = makeStyles((theme) => ({
   paper: {
-    marginTop: theme.spacing(8),
+    marginTop: theme.spacing(4),
+    marginBottom: theme.spacing(4),
+    padding: theme.spacing(4),
     display: 'flex',
     flexDirection: 'column',
     alignItems: 'center',
+    justifyContent: 'center'
+  },
+  title: {
+    marginTop: theme.spacing(8),
+    padding: theme.spacing(3),
+    "& h2": {
+      marginTop: theme.spacing(4)
+    }
   },
   avatar: {
     margin: theme.spacing(1),
@@ -44,6 +33,11 @@ const useStyles = makeStyles((theme) => ({
   form: {
     width: '100%', // Fix IE 11 issue.
     marginTop: theme.spacing(1),
+  },
+  centerText: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
   submit: {
     margin: theme.spacing(3, 0, 2),
@@ -75,34 +69,44 @@ export default function SignIn(loginHandler) {
     console.log('error :>> ', error);
   }
   return (
-    <Container component="main" maxWidth="xs">
+    <Container component="main" maxWidth="xl">
       <CssBaseline />
       <div className={classes.paper}>
-        <Avatar className={classes.avatar}>
-          <LockOutlinedIcon />
-        </Avatar>
-        <Typography component="h1" variant="h5">
-          Inicia de Sesión
-        </Typography>
-        <Box mt={5} >
-          <Typography justify="center" variant="subtitle1">
-            Solo para estudiantes colaboradores del Tec de Monterrey
+        <Grid container >
+          <Grid item md={6} className={classes.title}>
+          <Typography component="h1" variant="h2">
+            CE News
           </Typography>
-          <Box mt={2}>
+          <Typography component="h2" variant="subtitle1">
+            Eventos Estudiantiles
+          </Typography>
 
-            <GoogleLogin
-                clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}
-                buttonText="Inicio de sesión con Google"
-                onSuccess={_login}
-                onFailure={_loginFailure}
-                cookiePolicy={'single_host_origin'}
-            />
-          </Box>
-        </Box>
+          </Grid>
+          <Grid item md={6}>
+            <Paper className={classes.paper}>
+              <Typography component="h1" variant="h5">
+                Inicia de Sesión
+              </Typography>
+              <Box mt={5} width="60%">
+                <Typography variant="subtitle1" align="center">
+                  Solo para estudiantes colaboradores del Tec de Monterrey
+                </Typography>
+                <Box mt={2} className={classes.centerText}>
+
+                  <GoogleLogin
+                      clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}
+                      buttonText="Inicio de sesión con Google"
+                      onSuccess={_login}
+                      onFailure={_loginFailure}
+                      cookiePolicy={'single_host_origin'}
+                  />
+                </Box>
+              </Box>
+            </Paper>
+          </Grid>
+          
+        </Grid>
       </div>
-      <Box mt={8}>
-        <Copyright />
-      </Box>
     </Container>
   );
 }
