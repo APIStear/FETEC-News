ed2824be317f2ad40a1b16c8590960207ad9aae8
Adrian-Garcia <adriangarlop@hotmail.com>
2021-05-25 17:21:10 -0500

    Ya jalaaaaaaaaaaaaaaaaaaaaaaa

--- client/src/AllEvents.js
+++ client/src/AllEvents.js
@@ -1,53 +1,39 @@
 import React from 'react';
 import axios from "axios";
+import { useEffect, useState } from 'react';
 
-const initialState = {
-  title: "",
-  description: "",
-  startDate: new Date(),
-  imgKeys: [],
-  endDate: new Date(),
-  location: "",
-  isRSVP: false,
-  RSVPlist: [],
-  studentGroup: ""
-};
+// const initialState = [{
+//   title: "",
+//   description: "",
+//   startDate: new Date(),
+//   imgKeys: [],
+//   endDate: new Date(),
+//   location: "",
+//   isRSVP: false,
+//   RSVPlist: [],
+//   studentGroup: ""
+// }];
 
-export default function EventNew() {
+export default function AllEvents() {
   let url = process.env.REACT_APP_API_DOMAIN || "http://localhost:4000";
+  const [eventList, setEvents] = useState([]);
 
-  function _events() {
-    return axios.get(`${url}/api/events/`)
-      .then((response) => {
+  useEffect(() => {
+    axios.get(`${url}/api/events/`)
+      .then(response => {
         let events = response.data.events
-        return events.map((event) =>
+
+        let allEventsListed = events.map((event) =>
           <h2>{event.title}</h2>
         )
 
-        // return eventList;
-      })
-      .catch((error) => {
+        setEvents(allEventsListed)
+      }).catch(error => {
         console.log(error);
       });
-  }
-
-  let events = _events()
-
-
-  // let nums = [1,2,3] 
-  // let eventList = nums.map((event) =>
-  //   <h2>{event}</h2>
-  // )
+  });
 
   return (
-    events
-
-    // <h1>Hello</h1>
-
-    // <tbody>
-    //   {events.map(function(events, i){
-    //     return <Typography obj={events} key={i} >Hello</Typography>;
-    //   })}
-    // </tbody>
+    eventList
   );
 }
