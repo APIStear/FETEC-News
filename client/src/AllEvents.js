04f150ed4d2f2230ece2a221a6cbe80fd44f73dc
Adrian-Garcia <adriangarlop@hotmail.com>
2021-05-26 21:37:23 -0500

    Added link of individual event

--- client/src/AllEvents.js
+++ client/src/AllEvents.js
@@ -6,15 +6,12 @@ import './AllEvents.css';
 import { Button, Container, Grid, TextField, Checkbox } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import { format } from "date-fns";
-
-
-
+import { Link } from 'react-router-dom';
 
 // TODO: Esto solo puede ser accesado como admin
 export default function AllEvents() {
   let url = process.env.REACT_APP_API_DOMAIN || "http://localhost:4000";
   const [eventList, setEvents] = useState([]);
-
 
   useEffect(() => {
     axios.get(`${url}/api/events/`)
@@ -26,13 +23,16 @@ export default function AllEvents() {
           <div key={event.title}>
             <div className="EventCard">
               <div className="EventDate">
-                  <div className="EventDateDay">{new Intl.DateTimeFormat("es-MX", {
-                        day: "numeric",
-                      }).format(event.startDate)}</div>
-                  <div>{new Intl.DateTimeFormat("es-MX", {
-                      month: "long",
-                    }).format(event.startDate).toLocaleUpperCase()}</div>
-                {/* {event.startDate.getMonth()} */}
+                <div className="EventDateDay">
+                  {new Intl.DateTimeFormat("es-MX", {
+                    day: "numeric",
+                  }).format(event.startDate)}
+                </div>
+                <div>
+                  {new Intl.DateTimeFormat("es-MX", {
+                    month: "long",
+                  }).format(event.startDate).toLocaleUpperCase()}
+                </div>
               </div>
               <div className="EventTitleOrg">
                 <div className="EventTitle"> {event.title} </div>
@@ -42,17 +42,17 @@ export default function AllEvents() {
                 <div className="EventDetails"> {event.location} </div>
               </div>
               <div className="EventDetails">
-              <Button variant="contained" color="primary" disableElevation>
+              <Button
+                variant="contained"
+                color="primary"
+                component={Link}
+                to={`/event?eventId=${event._id}`}
+                disableElevation
+              >
                 Ver más
               </Button>
               </div>
             </div>
-            {/* <Typography component="h5" variant="h5">
-                <Typography component="body2" variant="body2">
-                  <div> Descripción: {event.description} </div>
-
-                </Typography>
-            </Typography> */}
             <hr></hr>
           </div>
       )})
