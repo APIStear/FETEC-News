4b148d923cf8025d600910fe1f0fc4e8be62ec63
Adrian-Garcia <adriangarlop@hotmail.com>
2021-05-26 21:24:13 -0500

    Added nats changes

--- client/src/AllEvents.js
+++ client/src/AllEvents.js
@@ -1,24 +1,38 @@
-import React from "react";
+import React from 'react';
 import axios from "axios";
 import { useEffect, useState } from 'react';
+import { Typography } from '@material-ui/core';
 import './AllEvents.css';
-import { Button } from '@material-ui/core';
+import { Button, Container, Grid, TextField, Checkbox } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
 import { format } from "date-fns";
+
+
+
 
 // TODO: Esto solo puede ser accesado como admin
 export default function AllEvents() {
   let url = process.env.REACT_APP_API_DOMAIN || "http://localhost:4000";
   const [eventList, setEvents] = useState([]);
 
+
   useEffect(() => {
     axios.get(`${url}/api/events/`)
       .then(response => {
         let events = response.data.events
-        let allEvents = events.map((event) =>
+        let allEvents = events.map((event) => {
+          event.startDate = new Date(event.startDate)
+          return (
           <div key={event.title}>
             <div className="EventCard">
               <div className="EventDate">
-                <div> inicio: {event.startDate} </div>
+                  <div className="EventDateDay">{new Intl.DateTimeFormat("es-MX", {
+                        day: "numeric",
+                      }).format(event.startDate)}</div>
+                  <div>{new Intl.DateTimeFormat("es-MX", {
+                      month: "long",
+                    }).format(event.startDate).toLocaleUpperCase()}</div>
+                {/* {event.startDate.getMonth()} */}
               </div>
               <div className="EventTitleOrg">
                 <div className="EventTitle"> {event.title} </div>
@@ -28,14 +42,20 @@ export default function AllEvents() {
                 <div className="EventDetails"> {event.location} </div>
               </div>
               <div className="EventDetails">
-              <Button variant="contained" color="primary" >
+              <Button variant="contained" color="primary" disableElevation>
                 Ver más
               </Button>
               </div>
             </div>
+            {/* <Typography component="h5" variant="h5">
+                <Typography component="body2" variant="body2">
+                  <div> Descripción: {event.description} </div>
+
+                </Typography>
+            </Typography> */}
             <hr></hr>
           </div>
-        )
+      )})
         setEvents(allEvents)
       }).catch(error => {
         console.log(error);
@@ -43,7 +63,8 @@ export default function AllEvents() {
   });
 
   return (
-    <div>
+    <div className="EventsMain" >
+      <h1>Próximos Eventos</h1>
       <div className="columnHeaders" >
         <h2 className="EventDate">Fecha</h2>
         <h2 className="EventTitleOrg">Evento</h2>
