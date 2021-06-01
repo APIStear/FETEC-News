ccf2013e779b9468641f3850e6ac8bc99c400b60
Memo Saldana <memo.sal98@gmail.com>
2021-06-01 03:32:47 -0700

    Creates new EventList component, and fixes main tag styles

--- client/src/AllEvents.js
+++ client/src/AllEvents.js
@@ -2,8 +2,7 @@ import React from 'react';
 import axios from "axios";
 import { useEffect, useState } from 'react';
 import './AllEvents.css';
-import { Button } from '@material-ui/core';
-import { Link } from 'react-router-dom';
+import EventList from './EventList';
 
 // TODO: Esto solo puede ser accesado como admin
 const AllEvents = ({ history, location }) => {
@@ -14,64 +13,18 @@ const AllEvents = ({ history, location }) => {
   useEffect(() => {
     axios.get(`${url}/api/events/`)
       .then(response => {
-        let events = response.data.events
-        let allEvents = events.map((event) => {
-          event.startDate = new Date(event.startDate)
-          return (
-          <div key={event.title}>
-            <div className="EventCard">
-              <div className="EventDate">
-                <div className="EventDateDay">
-                  {new Intl.DateTimeFormat("es-MX", {
-                    day: "numeric",
-                  }).format(event.startDate)}
-                </div>
-                <div>
-                  {new Intl.DateTimeFormat("es-MX", {
-                    month: "long",
-                  }).format(event.startDate).toLocaleUpperCase()}
-                </div>
-              </div>
-              <div className="EventTitleOrg">
-                <div className="EventTitle"> {event.title} </div>
-                <div className="EventDetails"> {event.studentGroup} </div>
-              </div>
-              <div className="EventLocation">
-                <div className="EventDetails"> {event.location} </div>
-              </div>
-              <div className="EventDetails">
-              <Button
-                variant="contained"
-                color="primary"
-                component={Link}
-                to={`/event?eventId=${event._id}`}
-                disableElevation
-              >
-                Ver más
-              </Button>
-              </div>
-            </div>
-            <hr></hr>
-          </div>
-      )})
-        setEvents(allEvents)
+        const events = response.data.events 
+        setEvents(events);
       }).catch(error => {
         history.push('/', {error:"Hubo un error"});
         console.log(error);
       });
-  }, [eventList, history, location]);
+  }, [eventList, history, location, url]);
 
   return (
     <div className="EventsMain" >
       <h1>Próximos Eventos</h1>
-      <div className="columnHeaders" >
-        <h2 className="EventDate">Fecha</h2>
-        <h2 className="EventTitleOrg">Evento</h2>
-        <h2 className="EventLocation">Lugar</h2>
-      </div>
-      <div className="EventsContainer">
-          {eventList}
-      </div>
+      <EventList events={eventList}/>
     </div>
   );
 }
---
+++ client/src/EventList.js
@@ -0,0 +1,72 @@
+import { Button, makeStyles } from "@material-ui/core";
+import { Link } from "react-router-dom";
+
+const useStyles = makeStyles({
+  linkText: {
+    textDecoration: `none`,
+    color: `white`
+  },
+})
+
+const EventList = ({events}) => {
+  const classes = useStyles();
+
+  return (
+    <div>
+
+      <div className="columnHeaders" >
+          <h2 className="EventDate">Fecha</h2>
+          <h2 className="EventTitleOrg">Evento</h2>
+          <h2 className="EventLocation">Lugar</h2>
+      </div>
+      <div className="EventsContainer">
+        
+        {
+          events.map((event) => {
+            event.startDate = new Date(event.startDate)
+            return (
+              <div key={event.title}>
+                <div className="EventCard">
+                  <div className="EventDate">
+                    <div className="EventDateDay">
+                      {new Intl.DateTimeFormat("es-MX", {
+                        day: "numeric",
+                      }).format(event.startDate)}
+                    </div>
+                    <div>
+                      {new Intl.DateTimeFormat("es-MX", {
+                        month: "long",
+                      }).format(event.startDate).toLocaleUpperCase()}
+                    </div>
+                  </div>
+                  <div className="EventTitleOrg">
+                    <div className="EventTitle"> {event.title} </div>
+                    <div className="EventDetails"> {event.studentGroup} </div>
+                  </div>
+                  <div className="EventLocation">
+                    <div className="EventDetails"> {event.location} </div>
+                  </div>
+                  <div className="EventDetails">
+                    <Button
+                      variant="contained"
+                      color="primary"
+                      component={Link}
+                      to={`/event?eventId=${event._id}`}
+                      disableElevation
+                      className={classes.linkText}
+                    >
+                      Ver más
+                    </Button>
+                  </div>
+                </div>
+                <hr></hr>
+              </div>
+            )
+          })
+        }
+      </div>
+    </div>
+  )
+}
+
+export default EventList;
